35f0f467a24a5e14a27d7d3460dafec7c70a362e
Kittu Jain <cskratijain@gmail.com>
2015-03-02 10:03:25 +0530

    refactored collatzChain using sets instead of map

--- CollatzChain.java
+++ CollatzChain.java
@@ -1,64 +1,40 @@
-import java.util.*;
+ import java.util.*;
 
 class CollatzChain {
-	// static Map<Integer, Integer> collatzMap = new HashMap<Integer, Integer>();
-
-	static class ChainComparator implements Comparator<Integer> {
-		public int compare(Integer left, Integer right) {
-			return (left-right);
-		}
-
-		public boolean equals(Object obj) {
-			return false;
-		}
-	}
-	static SortedSet<Integer> set = new TreeSet<Integer>(new ChainComparator());
+	static Set<Integer> set = new HashSet<Integer>();
 	public static void main(String[] args) {
-		set.add(2);
-		set.add(1);
-		set.add(4);
-		set.add(7);
-		set.add(7);
-		set.add(1);
-		set.add(9);
-		set.add(2);
-		set.add(0);
-		for (Integer i : set ) {
-			System.out.println(i);
+		int chainLength;
+		int longestChainLength = 0;
+		int startOfLongestChain = 0;
+		int max = 100000;
+		for (int i = max; i > 0; i--) {
+			if(set.contains(i)) continue;
+			chainLength = chainLength(i);
+			if (longestChainLength < chainLength) {
+				longestChainLength = chainLength;
+				startOfLongestChain = i;
+			}
 		}
-	// 	int chainLength;
-	// 	int longestChainLength = 0;
-	// 	int startOfLongestChain = 0;
-	// 	int max = 7;
-	// 	for (int i = 1; i < max; i++) {
-	// 		chainLength = chainLength(i);
-	// 		collatzMap.put(i, chainLength);
-	// 		if (longestChainLength < chainLength) {
-	// 			longestChainLength = chainLength;
-	// 			startOfLongestChain = i;
-	// 		}
-	}
 		
-	// 	System.out.println("Longest chain length upto " + max + " is " + longestChainLength +
-	// 	 " and it starts at " + startOfLongestChain);
-	// }
+		System.out.println("Longest chain length upto " + max + " is " + longestChainLength +
+		 " and it starts at " + startOfLongestChain);
+	}
 
-	// public static int chainLength(int start) {
-	// 	List<Integer> chain = new ArrayList<Integer>();
-	// 	chain.add(start);
-	// 	while (start > 1) {
-	// 		if(collatzMap.containsKey(start))
-	// 			return chain.size() + collatzMap.get(start);
-	// 		if (start % 2 == 0)
-	// 			start /= 2;
-	// 		else {
-	// 			start *= 3;
-	// 			start += 1;
-	// 		}
-	// 		chain.add(start);
-	// 	}
-	// 	return chain.size();
-	// }
+	public static int chainLength(int start) {
+		int count = 1;
+		set.add(start);
+		while (start > 1){
+			if (start % 2 == 0)
+				start /= 2;
+			else {
+				start *= 3;
+				start += 1;
+			}
+			count++;
+			set.add(start);
+		}
+		return count;
+	}
 }
 
 
